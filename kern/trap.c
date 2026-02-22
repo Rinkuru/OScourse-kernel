856cfa8499ca2155431835006a66bae06257317c
alexey <alexeych102@gmail.com>
2026-02-22 13:37:58 +0300

    Немного подкорректировал проверку на deadline miss

--- kern/trap.c
+++ kern/trap.c
@@ -352,10 +352,10 @@ trap_dispatch(struct Trapframe *tf) {
             // RUNNING:  процесс сейчас на CPU и не успел вызвать PERIODIC_WAIT
             // RUNNABLE: процесс готов, но планировщик его ещё не запустил
             if ((envs[i].env_status == ENV_RUNNING || envs[i].env_status == ENV_RUNNABLE) &&
-                now > envs[i].env_rt_absolute_deadline) {
+                get_current_time_us() > envs[i].env_rt_absolute_deadline) {
                 cprintf("[RT] DEADLINE MISS (trap_dispatch): Process %08x missed by %lu us\n", 
                     envs[i].env_id, 
-                    now - envs[i].env_rt_absolute_deadline
+                    get_current_time_us() - envs[i].env_rt_absolute_deadline
                 );
                 rt_handle_deadline_miss(&envs[i]);
             }
